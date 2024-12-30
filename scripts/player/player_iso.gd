ea9684ea201b8294474134cc5c55e914b9885337
Shan <89729321+Shinzx08@users.noreply.github.com>
2024-12-30 22:11:37 +0800

    normalized vector
    normalized the vector
    
    Co-Authored-By: ProfG1 <184029404+ProfG1@users.noreply.github.com>

--- scripts/player/player_iso.gd
+++ scripts/player/player_iso.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
 
 
-const  SPEED: float = 400.0
+const Speed = 400.0
 var motion = Vector2()
 var gun_cooldown = true
-var projectile = preload("res://scenes/projectiles/test_projectile.tscn")
 func convertCARTtoISO(cartesian):
 	return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y)/2)
 
@@ -13,17 +12,11 @@ func _physics_process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	$Marker2D.look_at(mouse_pos)
 	var input_vector = Input.get_vector("left", "right", "up", "down")
-	if Input.is_action_just_pressed("left_mouse") and gun_cooldown:
-		gun_cooldown = false
-		var projectile_instance = projectile.instantiate()
-		projectile_instance.rotation = $Marker2D.rotation
-		projectile_instance.global_position = $Marker2D.global_position
-		add_child(projectile_instance)
-		$gunCooldown.start()
+	var normalized_vector = input_vector.normalized()
 		
 		
 	
-	velocity = input_vector * SPEED
+	velocity = normalized_vector * Speed
 	#var direction = Vector2()
 	#if Input.is_action_just_pressed("up"):
 	#	direction += Vector2(0, -1)
