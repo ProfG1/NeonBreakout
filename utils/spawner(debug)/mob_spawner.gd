936777cc16d8b5fbd917e00fc3c6cad9bf12d241
Shan <89729321+Shinzx08@users.noreply.github.com>
2025-01-02 14:01:46 +0800

    Mob V1
    i forgor to add an action key
    
    Co-Authored-By: ProfG1 <184029404+ProfG1@users.noreply.github.com>

--- utils/spawner(debug)/mob_spawner.gd
+++ utils/spawner(debug)/mob_spawner.gd
@@ -10,7 +10,7 @@ var enemy_scene : PackedScene = preload("res://scenes/opps/enemy.tscn")
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("spawn_enemy"):
+	if Input.is_action_just_pressed("spawn_mob"):
 		var spawned_enemy : Enemy = enemy_scene.instantiate()
 		get_tree().root.add_child(spawned_enemy)
 		spawned_enemy.global_position = get_global_mouse_position()
