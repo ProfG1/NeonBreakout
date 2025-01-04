afd66405bce04ab5f25cbed19a7d6dce1f44bd92
ProfG1 <icewyvernthegreat@gmail.com>
2025-01-04 20:01:26 +0800

    Minor improvements to gun position

--- scripts/items/weapons/revolter.gd
+++ scripts/items/weapons/revolter.gd
@@ -3,17 +3,24 @@ extends Node2D
 
 @export var firing_position : Marker2D
 
-@onready var player : Player = get_owner()
+#@onready var player : Player = get_owner()
 
 var bullet_scene : PackedScene = preload("res://scenes/projectiles/test_projectile.tscn")
 
-
+func _process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	look_at(mouse_pos)
+	rotation_degrees = wrap(rotation_degrees, 0, 360)
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		scale.y = -1
+	else:
+		scale.y = 1
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		
 		# If we're aiming at a different side, flip the firing position across the X axis
-		if sign(player.aim_position.x) != sign(firing_position.position.x):
-			firing_position.position.x *= -1
+		#if sign(player.aim_position.x) != sign(firing_position.position.x):
+			#firing_position.position.x *= -1
 		
 		# Spawn a bullet and give it a rotation based on the angle between the firing position and
 		# the cursor's position.
